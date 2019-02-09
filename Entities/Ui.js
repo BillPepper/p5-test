59a1cd3ef2c96c2b5c26b709d95a3b105b1c8656
Mattes Wenzel <mattes@artfacts.net>
2019-02-10 00:22:43 +0100

    make menu a little prettier

--- Entities/Ui.js
+++ Entities/Ui.js
@@ -1,7 +1,30 @@
 class ui {
   renderMenu() {
     if (menuEnabled === true) {
+      // background
+      fill(20)
+      noStroke()
       rect(20, 20, resX - 40, resY - 40)
+
+      // border
+      stroke(200)
+      noFill()
+      rect(40, 90, resX - 80, resY - 130)
+
+      // seperator line
+      stroke(200)
+      line(resX / 2, 120, resY / 2, resY - 70)
+
+      // text
+      fill(200)
+      noStroke()
+      textSize(20)
+      text('Trade Menu', 40, 50)
+
+      textSize(15)
+      text('Your Items', 40, 80)
+      textSize(15)
+      text('Station Items', resX / 2, 80)
     }
   }
 
@@ -9,7 +32,6 @@ class ui {
     this.renderMenu()
     if (keyIsDown(ESCAPE)) {
       menuEnabled = false
-      console.log('esc')
     }
   }
 }
